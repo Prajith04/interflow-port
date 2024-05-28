a30f0e33508e40c7912a3752ff2e28ac3f2f34b6
KITE STUDENT <kitestudent@kgisledu.com>
2024-05-28 09:53:55 +0530

    modify

--- port/interflow_ai_port/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    # Add logic to check if user is logged in
-    if request.user.is_authenticated:
-        message = "Welcome back! You are logged in."
-    else:
-        message = "Welcome! Please log in to access more features."
-    return render(request, 'home.html', {'message': message})
